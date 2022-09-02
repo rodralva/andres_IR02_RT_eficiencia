732269d3818bd3b6ee0395aaf1cbdcd5ddb69c3b
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2022-09-02 10:09:05 +0200

    added data downloader script

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+#might need to ls /pnfs/ciemat.es/data/neutrinos/ first (pnfs no longer mounted by default, automount on use)
+
+scp rodrigoa@pcae145.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Eficiencia/GAr_Julio/ROOT/21_07_2022/run17_ch1.root .
+scp rodrigoa@pcae145.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Eficiencia/GAr_Julio/ROOT/21_07_2022/run10_ch1.root .
+scp rodrigoa@pcae145.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Eficiencia/GAr_Julio/ROOT/21_07_2022/run11_ch1.root .
+scp rodrigoa@pcae145.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Eficiencia/GAr_Julio/ROOT/21_07_2022/run09_ch1.root .
+scp rodrigoa@pcae145.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Eficiencia/GAr_Julio/ROOT/21_07_2022/run20_ch1.root .
